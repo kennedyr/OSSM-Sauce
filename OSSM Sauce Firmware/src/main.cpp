30ef76bc6da218adaa11386e4cc978d1ad4538b0
Eric Kennedy <eric.kennedy@openventana.com>
2025-07-08 21:31:54 -0400

    remove unused positionQueue

--- OSSM Sauce Firmware/src/main.cpp
+++ OSSM Sauce Firmware/src/main.cpp
@@ -16,9 +16,6 @@ QueueHandle_t moveQueue;
 const char moveQueueSize = 50;
 bool moveQueueIsEmpty = true;
 
-QueueHandle_t positionQueue;
-const char positionQueueSize = 50;
-bool positionQueueIsEmpty = true;
 int previousTargetPosition;
 
 StrokeCommand smoothMoveCommand;
@@ -200,7 +197,6 @@ void parseMessage(esp_websocket_event_data_t *data) {
       movementMode = MODE_IDLE;
       playTimeMs = 0;
       xQueueReset(moveQueue);
-      xQueueReset(positionQueue);
       moveQueueIsEmpty = true;
       break;
     }
@@ -341,7 +337,6 @@ void setup() {
   Serial.println("");
 
   moveQueue = xQueueCreate(moveQueueSize, 9);
-  positionQueue = xQueueCreate(positionQueueSize, 4);
 
   sensorlessHoming();
 
